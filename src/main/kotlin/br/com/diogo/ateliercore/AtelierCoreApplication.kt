349ec619fbe313bd8474a9199610723def319182
Diogo  Alberto <106744072+albertokff@users.noreply.github.com>
2026-01-17 09:24:25 -0300

    :sparkles: Initial Commit

---
+++ src/main/kotlin/br/com/diogo/ateliercore/AtelierCoreApplication.kt
@@ -0,0 +1,11 @@
+package br.com.diogo.ateliercore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtelierCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<AtelierCoreApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/diogo/ateliercore/AtelierCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.diogo.ateliercore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AtelierCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
